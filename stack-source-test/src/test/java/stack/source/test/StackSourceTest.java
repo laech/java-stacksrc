71c91793adbec7f7a5f8c9d605c6c6c8a19dbdb7
Lae <laec@protonmail.ch>
2018-04-25 23:11:06 +1200

    Update test.

--- stack-source-test/src/test/java/stack/source/test/StackSourceTest.java
+++ stack-source-test/src/test/java/stack/source/test/StackSourceTest.java
@@ -3,12 +3,15 @@ package stack.source.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import stack.source.internal.Decorator;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.function.Function;
 
+import static java.lang.String.join;
 import static java.lang.System.lineSeparator;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -20,17 +23,17 @@ public final class StackSourceTest {
     public static Collection<Object[]> data() {
         return asList(new Object[][]{
 
-                {new MultilineThrow(), new String[]{
+                {new MultilineThrow(), join(lineSeparator(),
                         "stack.source.test.TestException: hello world",
                         "\tat stack.source.test.MultilineThrow.run(MultilineThrow.java:7)",
                         "",
                         "\t-> 7          throw new TestException(",
                         "\t   8                  \"hello world\"",
                         "\t   9          );",
-                        "",
-                }},
+                        ""
+                ), null},
 
-                {new Chained(), new String[]{
+                {new Chained(), join(lineSeparator(),
                         "stack.source.test.TestException: what?",
                         "\tat stack.source.test.Chained.fail(Chained.java:15)",
                         "",
@@ -43,26 +46,26 @@ public final class StackSourceTest {
                         "\t    8                  .nothing1(\"blah\")",
                         "\t    9                  .nothing2(\"meh\")",
                         "\t-> 10                  .fail(\"what?\")",
-                        "",
-                }},
+                        ""
+                ), null},
 
-                {new ThrowExceptionCreatedElseWhere(), new String[]{
+                {new ThrowExceptionCreatedElseWhere(), join(lineSeparator(),
                         "stack.source.test.TestException: testing",
                         "\tat stack.source.test.ThrowExceptionCreatedElseWhere.run(ThrowExceptionCreatedElseWhere.java:8)",
                         "",
                         "\t-> 8          TestException test = new TestException(\"testing\");",
-                        "",
-                }},
+                        ""
+                ), null},
 
-                {new ThrowNewException(), new String[]{
+                {new ThrowNewException(), join(lineSeparator(),
                         "stack.source.test.TestException: testing",
                         "\tat stack.source.test.ThrowNewException.run(ThrowNewException.java:7)",
                         "",
                         "\t-> 7          throw new TestException(\"testing\");",
-                        "",
-                }},
+                        ""
+                ), null},
 
-                {new ReturnFailure(), new String[]{
+                {new ReturnFailure(), join(lineSeparator(),
                         "stack.source.test.TestException: testing",
                         "\tat stack.source.test.ReturnFailure.bye(ReturnFailure.java:15)",
                         "",
@@ -73,9 +76,9 @@ public final class StackSourceTest {
                         "\tat stack.source.test.ReturnFailure.run(ReturnFailure.java:7)",
                         "",
                         "\t-> 7          System.err.println(hi());",
-                        "",
-                }},
-                {new MultiCalls(), new String[]{
+                        ""
+                ), null},
+                {new MultiCalls(), join(lineSeparator(),
                         "stack.source.test.TestException: bob",
                         "\tat stack.source.test.MultiCalls.assertString(MultiCalls.java:12)",
                         "",
@@ -92,33 +95,44 @@ public final class StackSourceTest {
                         "\t   22          }));",
                         "",
                         "",
-                        "\tat java.util.Optional.ifPresent(Optional.java:159)",
+                        "\tat java.util.Optional.ifPresent(xxx)",
                         "\tat stack.source.test.MultiCalls.lambda$assertList$1(MultiCalls.java:17)",
-                        "\tat java.util.Arrays$ArrayList.forEach(Arrays.java:3880)",
+                        "\tat java.util.Arrays$ArrayList.forEach(xxx)",
                         "\tat stack.source.test.MultiCalls.assertList(MultiCalls.java:17)",
                         "\tat stack.source.test.MultiCalls.run(MultiCalls.java:27)",
                         "",
                         "\t-> 27          assertList(asList(\"bob\", \"bob\"));",
                         "",
-                        "",
-                }},
+                        ""
+                ), (Function<String, String>) s -> s
+                        // Temporary hack to make this work on Java 9 too
+                        .replaceAll("java.base/", "")
+                        .replaceAll("Optional\\.java:\\d+", "xxx")
+                        .replaceAll("Arrays\\.java:\\d+", "xxx")
+                        .replace("lambda$assertList$0", "lambda$null$0")},
         });
     }
 
-    private final Runnable test;
-    private final String expectedStackTraceBeginning;
+    @Parameter
+    public Runnable test;
 
-    public StackSourceTest(Runnable test, CharSequence[] expectedStackTraceBeginningLines) {
-        this.test = test;
-        this.expectedStackTraceBeginning = String.join(lineSeparator(), expectedStackTraceBeginningLines);
-    }
+    @Parameter(1)
+    public String expectedStackTraceBeginning;
+
+    @Parameter(2)
+    public Function<String, String> processor;
 
     @Test
     public void test() throws Exception {
         try {
             test.run();
         } catch (TestException e) {
-            String actual = print(e).substring(0, expectedStackTraceBeginning.length());
+
+            String actual = print(e);
+            if (processor != null) {
+                actual = processor.apply(actual);
+            }
+            actual = actual.substring(0, expectedStackTraceBeginning.length());
             assertEquals(expectedStackTraceBeginning, actual);
         }
     }
