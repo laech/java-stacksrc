b83974e15a988e310ae2be7689e396e808bef2b9
Lae <laec@protonmail.ch>
2018-04-29 19:29:33 +1200

    Update Javadoc.

--- stack-source-junit4/src/main/java/stack/source/junit4/ErrorDecorator.java
+++ stack-source-junit4/src/main/java/stack/source/junit4/ErrorDecorator.java
@@ -6,6 +6,42 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * Decorates stack traces with source code snippets.
+ * <p>
+ * Example output:
+ *
+ * <pre>
+ *
+ * decorated org.junit.ComparisonFailure: expected:&lt;H[ello]!&gt; but was:&lt;H[i]!&gt;
+ *     at org.junit.Assert.assertEquals(Assert.java:115)
+ *     at example.HelloTest.hello(HelloTest.java:16)
+ *
+ *        14      @Test
+ *        15      public void hello() {
+ *     -&gt; 16          assertEquals("Hello!", greet());
+ *        17      }
+ *
+ * ...
+ * </pre>
+ * <p>
+ * Usage:
+ *
+ * <pre>
+ *
+ * public class BaseTest {
+ *   &#x40;Rule
+ *   public final ErrorDecorator errorDecorator = new ErrorDecorator();
+ * }
+ *
+ * public final class MyTest extends BaseTest {
+ *   &#x40;Test
+ *   public void myTest() {
+ *     // ...
+ *   }
+ * }
+ * </pre>
+ */
 public final class ErrorDecorator implements TestRule {
 
     @Override
--- stack-source-junit5/src/main/java/stack/source/junit5/ErrorDecorator.java
+++ stack-source-junit5/src/main/java/stack/source/junit5/ErrorDecorator.java
@@ -6,6 +6,44 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.opentest4j.IncompleteExecutionException;
 
+/**
+ * Decorates stack traces with source code snippets.
+ * <p>
+ * Example output:
+ *
+ * <pre>
+ *
+ * decorated org.junit.ComparisonFailure: expected:&lt;H[ello]!&gt; but was:&lt;H[i]!&gt;
+ *     at org.junit.Assert.assertEquals(Assert.java:115)
+ *     at example.HelloTest.hello(HelloTest.java:16)
+ *
+ *        14      @Test
+ *        15      public void hello() {
+ *     -&gt; 16          assertEquals("Hello!", greet());
+ *        17      }
+ *
+ * ...
+ * </pre>
+ * <p>
+ * Usage:
+ *
+ * <pre>
+ *
+ * &#x40;ExtendWith(ErrorDecorator.class)
+ * class BaseTest {}
+ *
+ * class MyTest extends BaseTest {
+ *   &#x40;Test
+ *   void myTest() {
+ *     // ...
+ *   }
+ * }
+ * </pre>
+ * <p>
+ * Alternatively, run your tests with
+ * {@code -Djunit.jupiter.extensions.autodetection.enabled=true}
+ * instead of using annotations.
+ */
 @AutoService(Extension.class)
 public final class ErrorDecorator implements TestExecutionExceptionHandler {
 
