0484befe0033d2ae8d1280872ba8175cfb21ebe1
Lae Chen <laec@proton.me>
2023-09-30 13:01:49 +1300

    Cleanup dependencies.

--- junit5/src/test/java/nz/lae/stacksrc/junit5/ErrorDecoratorTest.java
+++ junit5/src/test/java/nz/lae/stacksrc/junit5/ErrorDecoratorTest.java
@@ -3,7 +3,9 @@ package nz.lae.stacksrc.junit5;
 import static java.lang.Math.min;
 import static java.lang.System.lineSeparator;
 import static nz.lae.stacksrc.core.Throwables.getStackTraceAsString;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.Disabled;
@@ -58,14 +60,14 @@ class ErrorDecoratorTest {
               lineSeparator(),
               "nz.lae.stacksrc.junit5.DecoratedAssertionError:",
               "org.opentest4j.AssertionFailedError: testing failure",
-              "\tat org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:36)",
-              "\tat org.junit.jupiter.api.Assertions.fail(Assertions.java:62)",
-              "\tat nz.lae.stacksrc.junit5.ErrorDecoratorTest.decoratesFailure(ErrorDecoratorTest.java:20)",
+              "\tat org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:38)",
+              "\tat org.junit.jupiter.api.Assertions.fail(Assertions.java:134)",
+              "\tat nz.lae.stacksrc.junit5.ErrorDecoratorTest.decoratesFailure(ErrorDecoratorTest.java:22)",
               "",
-              "\t   18    @Test",
-              "\t   19    void decoratesFailure() {",
-              "\t-> 20      fail(\"testing failure\");",
-              "\t   21    }",
+              "\t   20    @Test",
+              "\t   21    void decoratesFailure() {",
+              "\t-> 22      fail(\"testing failure\");",
+              "\t   23    }",
               "");
       assertEquals(DecoratedAssertionError.class, e.getClass());
       assertStackTrace(expected, e);
@@ -77,18 +79,19 @@ class ErrorDecoratorTest {
               lineSeparator(),
               "nz.lae.stacksrc.junit5.DecoratedAssertionError:",
               "org.opentest4j.AssertionFailedError: array contents differ at index [0], expected: <1> but was: <2>",
-              "\tat org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:36)",
-              "\tat org.junit.jupiter.api.AssertArrayEquals.failArraysNotEqual(AssertArrayEquals.java:434)",
-              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:246)",
-              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:92)",
-              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:88)",
-              "\tat org.junit.jupiter.api.Assertions.assertArrayEquals(Assertions.java:622)",
-              "\tat nz.lae.stacksrc.junit5.ErrorDecoratorTest.decoratesArrayFailure(ErrorDecoratorTest.java:25)",
+              "\tat org.junit.jupiter.api.AssertionFailureBuilder.build(AssertionFailureBuilder.java:151)",
+              "\tat org.junit.jupiter.api.AssertionFailureBuilder.buildAndThrow(AssertionFailureBuilder.java:132)",
+              "\tat org.junit.jupiter.api.AssertArrayEquals.failArraysNotEqual(AssertArrayEquals.java:440)",
+              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:241)",
+              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:87)",
+              "\tat org.junit.jupiter.api.AssertArrayEquals.assertArrayEquals(AssertArrayEquals.java:83)",
+              "\tat org.junit.jupiter.api.Assertions.assertArrayEquals(Assertions.java:1277)",
+              "\tat nz.lae.stacksrc.junit5.ErrorDecoratorTest.decoratesArrayFailure(ErrorDecoratorTest.java:27)",
               "",
-              "\t   23    @Test",
-              "\t   24    void decoratesArrayFailure() {",
-              "\t-> 25      assertArrayEquals(new int[] {1}, new int[] {2});",
-              "\t   26    }",
+              "\t   25    @Test",
+              "\t   26    void decoratesArrayFailure() {",
+              "\t-> 27      assertArrayEquals(new int[] {1}, new int[] {2});",
+              "\t   28    }",
               "");
       assertEquals(DecoratedAssertionError.class, e.getClass());
       assertStackTrace(expected, e);
