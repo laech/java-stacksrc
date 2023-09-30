7baa198fc095de6b5f4662443f1c09cc3f8731a4
Lae Chen <laec@proton.me>
2023-10-01 12:24:17 +1300

    Add test.

---
+++ core/src/test/java/nz/lae/stacksrc/core/MultilineThrowTest.java
@@ -0,0 +1,32 @@
+package nz.lae.stacksrc.core;
+
+import static nz.lae.stacksrc.core.Assertions.assertStackTrace;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class MultilineThrowTest {
+
+  private void doThrow() {
+    throw new AssertionError( //
+        "hello world" //
+        );
+  }
+
+  @Test
+  void run() {
+    var exception = assertThrows(AssertionError.class, this::doThrow);
+    var expected =
+        """
+java.lang.AssertionError: hello world
+	at nz.lae.stacksrc.core.MultilineThrowTest.doThrow(MultilineThrowTest.java:11)
+
+	   10    private void doThrow() {
+	-> 11      throw new AssertionError( //
+	   12          "hello world" //
+	   13          );
+
+""";
+    assertStackTrace(expected, exception);
+  }
+}
