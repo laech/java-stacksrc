27b552be498700c94db0099c760130f30dcb161c
Lae Chen <laec@proton.me>
2023-12-07 15:06:18 +1300

    Remove kotlin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
   myproject.`java-conventions`
   myproject.`library-conventions`
   `java-test-fixtures`
-  alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
   testImplementation(libs.junit.jupiter)
-  testImplementation(libs.kotlin.stdlib)
   testFixturesImplementation(libs.junit.jupiter.api)
   testFixturesImplementation(libs.jaxb.runtime)
 }
